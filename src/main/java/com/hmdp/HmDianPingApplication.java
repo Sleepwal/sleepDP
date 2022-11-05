8091e3780b934b55ffe849336483c32f4a279c84
SleepWalker <SleepWalker@123.com>
2022-11-05 17:20:14 +0800

    修复拦截器拦截swagger2的问题

--- src/main/java/com/hmdp/HmDianPingApplication.java
+++ src/main/java/com/hmdp/HmDianPingApplication.java
@@ -3,9 +3,12 @@ package com.hmdp;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@MapperScan("com.hmdp.mapper")
+
 @SpringBootApplication
+@MapperScan("com.hmdp.mapper")
 public class HmDianPingApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/hmdp/config/MvcConfig.java
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -3,6 +3,7 @@ package com.hmdp.config;
 import com.hmdp.utils.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -26,6 +27,20 @@ public class MvcConfig implements WebMvcConfigurer {
                         "/blog/hot",
                         "/user/code",
                         "/user/login"
-                );
+                )
+                // swagger-ui
+                .excludePathPatterns("/swagger-resources/**", "/webjars/**",
+                        "/v2/**", "/swagger-ui.html/**");
     }
+
+    // 必须添加
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+    }
+
 }
